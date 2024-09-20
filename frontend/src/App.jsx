6a0de851d316d765290e413b2869a005f1144c73
suman123 <sumanbiswas6854@gmail.com>
2024-09-20 19:19:24 +0530

    completefooter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
 import LandingPage from './components/landing/LandingPage'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
